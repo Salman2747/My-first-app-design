edc57271daa3fe4edd292ae7edabb2c7b06c5474
Salman2747 <ahmedsalman5136@gmail.com>
2021-11-03 08:25:53 +0600

    initial commit

--- lib/messenger_page.dart
+++ lib/messenger_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_design/profile%20page.dart';
 
 class MessengerPage extends StatefulWidget {
   const MessengerPage({Key key}) : super(key: key);
@@ -12,7 +13,49 @@ class _MessengerPageState extends State<MessengerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+appBar: AppBar(
+  leading: IconButton(
+    icon: Icon(
+      Icons.arrow_back_ios_outlined,
+      color: Color.fromRGBO(25, 25, 35, .6),
+    ),
+    iconSize: 25,
+    onPressed:() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+        builder: (context)=>ProfilePage(),
+      ),
+      );
+    } ,
+  ),
+  title: Text(
+    "Messenger Page",
+    style: TextStyle(
+      color: Color.fromRGBO(25, 25, 25, .6),
+      fontSize: 25,
+      fontWeight: FontWeight.w700,
 
+    ),
+  ),
+  actions: [
+    IconButton(icon:Icon(
+      Icons.refresh_outlined,
+color: Color.fromRGBO(25, 25, 35, .6),
+    ) ,
+        iconSize:25 ,
+        onPressed:() { Navigator.push(context, MaterialPageRoute(builder: (context)=>MessengerPage(),
+        ),
+        );
+    },
+    ),
+  ],
+),
+      body: Center(
+        child: Text(
+          "Messenger Page"
+        ),
+      ),
     );
   }
 }
