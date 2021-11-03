2f09130853b2a1a70bb1704dea4983e167d4deee
Salman2747 <ahmedsalman5136@gmail.com>
2021-11-03 07:54:41 +0600

    initial commit

--- lib/follower_page.dart
+++ lib/follower_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_design/profile%20page.dart';
 
 class Follower_page extends StatefulWidget {
   const Follower_page({Key key}) : super(key: key);
@@ -12,7 +13,50 @@ class _Follower_pageState extends State<Follower_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+appBar: AppBar(
+  title: Text(
+    "Follower Page",
+    style: TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.w700,
+      color: Color.fromRGBO(25, 25, 35, .6),
+    ),
+  ),
+  centerTitle: true,
+  leading: IconButton(
+    icon: Icon(Icons.arrow_back_ios_outlined,
+    color: Color.fromRGBO(25, 25, 35, .6),
+    ),
+    iconSize:25 ,
+    onPressed: () {
+      Navigator.push(context,
+        MaterialPageRoute(
+        builder: (context)=>ProfilePage(),
+      ),
+      );
+    },
+  ),
+  actions: [
+    IconButton(icon:Icon(Icons.refresh_outlined,
+    color: Color.fromRGBO(25, 25, 35, .6),
+    ) ,
+        iconSize:25 ,
+        onPressed: () {Navigator.push(
+          context,
+          MaterialPageRoute(
+          builder: (context)=>Follower_page(),
+          ),
+        );
+        },
+    ),
+  ],
+),
+      backgroundColor: Color.fromRGBO(240, 240, 240, .6),
+      body: Center(
+        child: Text(
+          "Follower Page"
+        ),
+      ),
     );
   }
 }
